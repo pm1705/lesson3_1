e62310befd76795fed71b90fe4d61f02a917f2d6
Administrator <pm1705@bs.amalnet.k12.il>
2021-01-04 14:29:09 +0200

    last for 3_1

--- app/src/main/java/com/example/lesson3_1/MainActivity.java
+++ app/src/main/java/com/example/lesson3_1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.lesson3_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+    }
+
+    public void clicked(View view) {
+        btn.setText("Ive been clicked");
     }
 }
